acbcf7b0eb248017b706fa1877a9c515ed119000
Ireme-Nyawe <akimanaj41@gmail.com>
2024-03-02 20:05:53 +0200

    add Event handler

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,13 +1,16 @@
+import { MouseEvent } from "react";
 function ListGroup() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-  items = [];
+  const handleClick = (event: MouseEvent) => console.log(event.clientX);
   return (
     <>
       <h1>List</h1>
       <ul className="list-group">
         {items.length === 0 && <p>No Data Found!</p>}
         {items.map((item) => (
-          <li key={item}>{item}</li>
+          <li className="list-group-item" key={item} onClick={handleClick}>
+            {item}
+          </li>
         ))}
       </ul>
     </>
