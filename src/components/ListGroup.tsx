d58d35bf1bfc2efbd8397e0a21b9090ade0960ae
Ireme-Nyawe <akimanaj41@gmail.com>
2024-03-02 20:24:16 +0200

    apply Hooks function/useState

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,14 +1,25 @@
-import { MouseEvent } from "react";
+import { useState } from "react";
+
 function ListGroup() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-  const handleClick = (event: MouseEvent) => console.log(event.clientX);
+  const [selectedIndex, setSelectedIndex] = useState(-1);
   return (
     <>
       <h1>List</h1>
       <ul className="list-group">
         {items.length === 0 && <p>No Data Found!</p>}
-        {items.map((item) => (
-          <li className="list-group-item" key={item} onClick={handleClick}>
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
+          >
             {item}
           </li>
         ))}
