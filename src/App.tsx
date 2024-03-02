98f96b06e740294cd747992cc481087368f79321
Ireme-Nyawe <akimanaj41@gmail.com>
2024-03-02 22:38:52 +0200

    passsing child component prop

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
+import Alert from "./components/Alert";
 import ListGroup from "./components/ListGroup";
 let items = ["New York", "San Francisco", "Tokyo", "Kigali", "Paris"];
-const handleSelectItem = (item:string) => {
-  console.log(item)
+const handleSelectItem = (item: string) => {
+  console.log(item);
 };
 function App() {
   return (
     <div>
-      <ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem} />
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      />
+      <Alert>
+        <div className="alert">
+          <h1>Alert</h1>
+          <p>Alert-Text</p>
+        </div>
+      </Alert>
     </div>
   );
 }
